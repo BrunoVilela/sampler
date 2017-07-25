5b9daee290237d9566cc4b0ec98fbf4e62a297df
Bruno Vilela <bvilela@boteromacserver2.wustl.edu>
2017-07-25 15:00:40 -0500

    vignette

--- R/run_sampler.R
+++ R/run_sampler.R
@@ -51,9 +51,9 @@
 #' ## Calculate the distance
 #' dist <- cophenetic(tree)
 #' ## Highly overdispersed 50% resample design (alpha = 50)
-#' selection <- run_sampler(x = dist, n = 5, alpha = 100, starting = "t10")
-#' ## Highly aggregated 50% resample design (alpha = -100)
-#' selection2 <- run_sampler(x = dist, n = 5, alpha = -100, starting = "t10")
+#' selection <- run_sampler(x = dist, n = 5, alpha = 50, starting = "t10")
+#' ## Highly aggregated 50% resample design (alpha = -50)
+#' selection2 <- run_sampler(x = dist, n = 5, alpha = -50, starting = "t10")
 #' ## Random 50% resample design (alpha = 0)
 #' selection3 <- run_sampler(x = dist, n = 5, alpha = 0, starting = "t10")
 #' ## Plot to compare
